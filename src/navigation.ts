9131bbd5e8a389e7dc0446bce36a71ab01043998
Christopher Prainito <christopherprainito@college.harvard.edu>
2025-04-07 20:00:10 -0400

    Change favicons, add donation page and embed, and add Google Tag

--- src/navigation.ts
+++ src/navigation.ts
@@ -15,12 +15,21 @@ export const headerData = {
       href: getPermalink('/registration'),
     },
     {
-      text: 'Events',
-      href: getPermalink('/events'),
+      text: 'Our Activities',
+      links: [
+        {
+          text: 'Events',
+          href: getPermalink('/events'),
+        },
+        {
+          text: 'Scholarships',
+          href: getPermalink('/scholarships'),
+        },
+      ],
     },
     {
-      text: 'Scholarships',
-      href: getPermalink('/scholarships'),
+      text: 'Donate',
+      href: getPermalink('/donate'),
     },
     {
       text: 'Contact',
@@ -48,7 +57,10 @@ export const footerData = {
     },
     {
       title: 'Get Involved',
-      links: [{ text: 'Member Registration', href: '/registration' }],
+      links: [
+        { text: 'Member Registration', href: '/registration' },
+        { text: 'Donate', href: '/donate' },
+      ],
     },
   ],
   secondaryLinks: [
@@ -62,6 +74,6 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://christopherprainito.com/"> Christopher Prainito '22</a> · All rights reserved.
+    Designed by <a class="text-blue-600 underline dark:text-muted" href="https://christopherprainito.com/"> Christopher Prainito '22</a> · All rights reserved.
   `,
 };
